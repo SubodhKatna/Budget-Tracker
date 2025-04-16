8d85078a5bfb88af061bf36760d6ccdf5aabf730
SubodhKatna <subodhkatna@gmail.com>
2025-04-16 23:44:11 +0530

    Start to work on the ci-cd workflow using github action.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,34 +1,28 @@
+const { headers } = require('next/headers');
+
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
     extends: [],
-    plugins: [
-      {
-        rules: {
-          'header-min-length': ({ header }) => {
-            return [
-              header.length >= 20,
-              'Commit message must be at least 20 characters long.',
-            ];
-          },
-          'header-case-start-capital': ({ header }) => {
-            return [
-              header[0] === header[0]?.toUpperCase(),
-              'Commit message must start with a capital letter.',
-            ];
-          },
-          'header-end-period': ({ header }) => {
-            return [
-              header.trim().endsWith('.'),
-              'Commit message must end with a period.',
-            ];
-          },
-        },
-      },
-    ],
     rules: {
       'header-min-length': [2, 'always'],
       'header-case-start-capital': [2, 'always'],
       'header-end-period': [2, 'always'],
     },
+    plugins: [
+      {
+        rules: {
+          'header-case-start-capital': ({raw}) => {
+            return [
+              /^[A-Z]/.test(raw),
+              'Commit message must start with a capital letter',
+            ]
+          },
+          'header-end-period': ({header}) => {
+            return [/\.$/.test(header), 'Commit message should end with a period'];
+          }
+        }
+      }
+    ]
+
   };
   
