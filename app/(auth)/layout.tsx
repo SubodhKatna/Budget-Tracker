f424c6ca9a5075b0b37abf87118ffa4e14344aec
SubodhKatna <subodhkatna@gmail.com>
2025-04-22 16:26:45 +0530

    Done.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import Logo from '@/components/Logo';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  // const notUsing = 1;
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
       <Logo />
